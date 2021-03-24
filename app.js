98cf9baa6ccd7d34cd85a0bbdd2659835832126e
Manuel Lopez <lopez1manuel@gmail.com>
2021-03-23 20:58:31 -0700

    Attemping to add images and info using JQuery from an API

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 $(document).ready(function() {
-    $.ajax()
+    var apiKey = "cda6ba4445ba68b5cb8199c475d634a56a54eb26";
+    console.log("test");
+    $("#button").click(function() {
+        // alert("you clicked a button!")
+        $.get(`https://comicvine.gamespot.com/api/characters/?api_key=${apiKey}&filter=name:spider-woman&format=json`,function (first) {
+        var htmlStr = "";
+        console.log("test");
+        // var pic = $(first.results.screen_url).text();
+        console.log(response);
+        // htmlStr += `<img src='${pic}'alt='SpiderWoman Test Image'>`
+        });
+        // $("#charImage").append(htmlStr);
+    });
+    
+    
+    
+    // var charVal = $("#charName").text();
+    // var url = `https://comicvine.gamespot.com/api/characters/?api_key=${apiKey}&filter=name:${charVal}&format=json`;
+
+    // Character Description
+    // $("form").submit(function () {
+    //     $.get(url, function (getChar) {
+    //         var results = getChar.results
+    //         var htmlStr = "";
+    //         console.log(test)
+    //         for(var i = 0; i < results.length; i++) {
+    //             console.log(test)
+    //             htmlStr += `<h3>${results[i].name}</h3>`;
+    //         } 
+    //         $("#charInfo").append(htmlStr);
+    //     });
+    //     return false;
+    // });
+
 });
+// var spiderwoman = 1453;
